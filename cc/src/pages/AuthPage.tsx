eda1b1eb8ec9dd8e63cd4206d3fb3fe371a6d4e1
kdten <kolten.rasmussen@gmail.com>
2023-12-22 14:16:00 -0700

    page flexing, trying layoutshift prevention

--- cc/src/pages/AuthPage.tsx
+++ cc/src/pages/AuthPage.tsx
@@ -6,49 +6,55 @@ import { IonContent, IonImg, IonText } from "@ionic/react";
 
 const AuthPage: React.FC = () => {
   const [isLogin, setIsLogin] = useState(true);
+
   
   const styles: { [key: string]: CSSProperties } = {
     contentContainer: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100%',
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "flex-start", // align content to the top
+      height: "100%",
     },
     logoImage: {
       maxWidth: 700,
       minWidth: 300,
-        width: '100%',
-        height: 'auto',
-        margin: '0 auto'
+      width: "100%",
+      height: "auto",
+      margin: "0 auto",
     },
     authContainer: {
       maxWidth: 270,
-      margin: '0 auto',
-marginTop: '-40px'
+      margin: "0 auto",
+      marginTop: "-40px",
     },
     footerText: {
-      display: 'block',
-      textAlign: 'center',
+      display: "block",
+      textAlign: "center",
       marginTop: 20,
       fontSize: 12,
-      position: 'absolute',
+      position: "absolute",
       bottom: 20,
-      left: '50%',
-      transform: 'translateX(-50%)',
-      whiteSpace: 'nowrap'
-    }
+      left: "50%",
+      transform: "translateX(-50%)",
+      whiteSpace: "nowrap",
+    },
   };
 
   return (
     <IonContent fullscreen class="ion-padding" style={styles.contentContainer}>
       {/* Logo */}
       {/* <div style={styles.logoImageWrapper}> */}
-      <IonImg src="v3.png" class="ion-padding" style={styles.logoImage} alt="A cartoon fish facing a hook where the fin undereneatht eh fish and the hook from the two C's to Coupon Catch in this logo" />
-    {/* </div> */}
+      <IonImg
+        src="v3.png"
+        class="ion-padding"
+        style={styles.logoImage}
+        alt="A cartoon fish facing a hook where the fin undereneatht eh fish and the hook from the two C's to Coupon Catch in this logo"
+      />
+      {/* </div> */}
 
-<div style={styles.authContainer}>
-      {isLogin ? (
+      <div style={styles.authContainer}>
+        {isLogin ? (
           <LoginComponent toggleLogin={() => setIsLogin(false)} />
         ) : (
           <SignUpComponent toggleLogin={() => setIsLogin(true)} />
