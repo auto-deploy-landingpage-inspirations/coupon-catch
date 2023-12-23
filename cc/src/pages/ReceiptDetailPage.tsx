d48850fa787299f8b121aad1e79d7525deb54e0e
kdten <kolten.rasmussen@gmail.com>
2023-12-23 10:17:39 -0700

    formatting

--- cc/src/pages/ReceiptDetailPage.tsx
+++ cc/src/pages/ReceiptDetailPage.tsx
@@ -24,16 +24,20 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import React, { Suspense, useCallback, useEffect, useMemo, useState } from "react";
+import React, {
+  Suspense,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { useParams } from "react-router-dom";
 import { AuthStore } from "../utils/store";
 
 import DemoUINotice from "../components/DemoUINotice";
 import "../styles/ReceiptDetailPage.css";
 import { type } from "os";
-import {
-  trashOutline,
-} from "ionicons/icons";
+import { trashOutline } from "ionicons/icons";
 import { db, deleteReceiptInFb, markItemsRedeemed } from "../utils/fbFirestore";
 import { doc, deleteDoc } from "firebase/firestore";
 import { ButtonContent } from "../components/ButtonContent";
@@ -44,8 +48,11 @@ import { useStoreState } from "pullstate";
 import { ReceiptStore } from "../utils/store";
 import { CouponStore } from "../utils/store";
 import NoEligibleItemsCard from "../components/NoEligibleItemsCard";
+import { hi } from "date-fns/locale";
 // import EligibleItemsCard from "../components/EligibleItemsCard";
-const EligibleItemsCard = React.lazy(() => import('../components/EligibleItemsCard'));
+const EligibleItemsCard = React.lazy(
+  () => import("../components/EligibleItemsCard")
+);
 
 const ReceiptDetailPage: React.FC = () => {
   const selectUid = (state: IAuthStore) => state.uid;
@@ -61,69 +68,70 @@ const ReceiptDetailPage: React.FC = () => {
   const isDemoUser = AuthStore.useState((s) => s.isDemoUser);
   const [unpaidShowAlert, setUnpaidShowAlert] = useState(false);
 
-
   // Find the receipt with the matching ID
-  const receipt = useMemo(() => receipts.find((r) => r.id == receiptId), [receipts, receiptId]);
+  const receipt = useMemo(
+    () => receipts.find((r) => r.id == receiptId),
+    [receipts, receiptId]
+  );
 
   // TODO: change this logic to wait for if the receipts have been loaded. If they have not we should display a skeleton loader
   if (!receipt) {
     return <NoReceiptDetailPage />; // Or some other error handling
   }
 
-  const handleCheckout = useCallback(async (firebaseUserId: string, receiptId: string) => {
-    try {
-      const response = await fetch(
-        "http://localhost:3000/create-checkout-session",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ firebaseUserId, receiptId }),
-        }
-      );
-      const session = await response.json();
-      window.location.href = session.url;
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }, []);
-
-
+  const handleCheckout = useCallback(
+    async (firebaseUserId: string, receiptId: string) => {
+      try {
+        const response = await fetch(
+          "http://localhost:3000/create-checkout-session",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ firebaseUserId, receiptId }),
+          }
+        );
+        const session = await response.json();
+        window.location.href = session.url;
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    },
+    []
+  );
 
   const handleOKOnUnlockAlert = useCallback(async () => {
     // If the receipt ID is "FcoblaFPX5PFjQcWtkUh" and the user is a demo user,
     // unlock the coupons without going to checkout
     if (receipt.id === "FcoblaFPX5PFjQcWtkUh" && isDemoUser) {
-      
       ReceiptStore.update((s) => {
         const receiptIndex = s.receiptList.findIndex((r) => r.id === receiptId);
         if (receiptIndex !== -1) {
           s.receiptList[receiptIndex].isUnlocked = true;
         }
       });
-    } 
+    }
     // If the receipt ID is "GLME3VtKiKPUXhxVVWdA", then trigger the checkout
     else if (receipt.id === "GLME3VtKiKPUXhxVVWdA" && isDemoUser) {
       await handleCheckout(uid, receiptId);
 
       setTimeout(() => {
-
-      ReceiptStore.update((s) => {
-        const receiptIndex = s.receiptList.findIndex((r) => r.id === receiptId);
-        if (receiptIndex !== -1) {
-          s.receiptList[receiptIndex].isUnlocked = true;
-        }
-      });
-    }, 5000);
-
-    } 
+        ReceiptStore.update((s) => {
+          const receiptIndex = s.receiptList.findIndex(
+            (r) => r.id === receiptId
+          );
+          if (receiptIndex !== -1) {
+            s.receiptList[receiptIndex].isUnlocked = true;
+          }
+        });
+      }, 5000);
+    }
     // For all other cases (including non-demo users), trigger the checkout and unlock process
     else {
       await handleCheckout(uid, receiptId);
     }
   }, [uid, receiptId, isDemoUser]);
-  
 
   const handleDeleteReceipt = useCallback(async () => {
     setLoadingFor("deletebutton");
@@ -134,7 +142,6 @@ const ReceiptDetailPage: React.FC = () => {
         const receiptRef = doc(db, "receipts", receiptId);
         // Mark the Receipt and its itemLines deleted
         await deleteReceiptInFb(receiptId);
-
       } else {
         // If the user is a demo user, delete the receipt from the array
         ReceiptStore.update((s) => {
@@ -171,28 +178,22 @@ const ReceiptDetailPage: React.FC = () => {
         <IonTitle>Receipt {receipt.dateOfPurchase}</IonTitle>
       </IonToolbar>
 
-
-
       <div>
         <IonContent className="background-image">
-        <Suspense fallback={<IonSpinner />}>
-        {
-          receipt.daysLeft > 0 && receipt.unlockCouponTotal > 0 ? (
-            <EligibleItemsCard
-              receipt={receipt}
-              couponList={couponList}
-              setUnpaidShowAlert={setUnpaidShowAlert}
-              setShowRedeemedAlert={setShowRedeemedAlert}
-            />
-          ) : (
-            <NoEligibleItemsCard receipt={receipt} />
-          )
-        }
-        </Suspense>
- 
+          <Suspense fallback={<IonSpinner />}>
+            {receipt.daysLeft > 0 && receipt.unlockCouponTotal > 0 ? (
+              <EligibleItemsCard
+                receipt={receipt}
+                couponList={couponList}
+                setUnpaidShowAlert={setUnpaidShowAlert}
+                setShowRedeemedAlert={setShowRedeemedAlert}
+              />
+            ) : (
+              <NoEligibleItemsCard receipt={receipt} />
+            )}
+          </Suspense>
 
-
-        {/* Alert for when the user clicks the unlock button */}
+          {/* Alert for when the user clicks the unlock button */}
           <IonAlert
             onDidDismiss={() => setUnpaidShowAlert(false)}
             isOpen={unpaidShowAlert}
@@ -213,7 +214,7 @@ const ReceiptDetailPage: React.FC = () => {
                 handler: () => {
                   handleOKOnUnlockAlert(); // Call the async function
                 },
-              }  
+              },
             ]}
           ></IonAlert>
 
@@ -222,7 +223,7 @@ const ReceiptDetailPage: React.FC = () => {
             isOpen={showRedeemedAlert}
             header="Congratulations!"
             // subHeader="A Sub Header Is Optional"
-            message="We've added this to your total savings."
+            message="We've added this to your total savings!"
             buttons={[
               {
                 text: "OK",
@@ -230,16 +231,38 @@ const ReceiptDetailPage: React.FC = () => {
                 handler: async () => {
                   // create list of item IDs to mark as redeemed
                   const itemIdArray = receipt.itemLines
-                  .filter(itemLine => couponList.some(coupon => coupon.itemNumber === itemLine.itemNumber))
-                  .map(itemLine => itemLine.id);
+                    .filter((itemLine) =>
+                      couponList.some(
+                        (coupon) => coupon.itemNumber === itemLine.itemNumber
+                      )
+                    )
+                    .map((itemLine) => itemLine.id);
 
-                    await markItemsRedeemed(itemIdArray);
-                    await handleDeleteReceipt();
-                    // remove the receipt from the store
+                  if (isDemoUser) {
+                    // If the user is a demo user, mark the receipt as redeemed locally.
+                    // mark the items as redeemed locally
                     ReceiptStore.update((s) => {
-                      s.receiptList = s.receiptList.filter((r) => r.id !== receiptId);
+                      const receiptIndex = s.receiptList.findIndex(
+                        (r) => r.id === receiptId
+                      );
+                      if (receiptIndex !== -1) {
+                        s.receiptList[receiptIndex].isRedeemed = true;
+                      }
+                      history.push("/dashboard/home");
                     });
-                  history.push("/dashboard/home");
+                  } else {
+                    // mark items as redeemed in firestore
+                    await markItemsRedeemed(itemIdArray);
+                    // delete receipt from firestore
+                    await handleDeleteReceipt();
+                    // remove the receipt from the local store
+                    ReceiptStore.update((s) => {
+                      s.receiptList = s.receiptList.filter(
+                        (r) => r.id !== receiptId
+                      );
+                    });
+                    history.push("/dashboard/home");
+                  }
                 },
               },
             ]}
