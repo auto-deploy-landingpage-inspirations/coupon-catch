0d4402401bca040ac58cf405cbc49fac5845c04d
kdten <kolten.rasmussen@gmail.com>
2023-12-25 14:58:35 -0700

    formatting/loading

--- cc/src/components/LoadingPage.tsx
+++ cc/src/components/LoadingPage.tsx
@@ -1,25 +1,32 @@
-import { IonContent, IonHeader, IonList, IonLoading, IonTitle, IonToolbar } from '@ionic/react'
-import React, { useState } from 'react'
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonLoading,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import React, { useState } from "react";
 
 const LoadingPage = () => {
-    const [showLoading, setShowLoading] = useState(true);
+  const [showLoading, setShowLoading] = useState(true);
 
   return (
     <IonContent fullscreen>
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle>
-        <IonTitle>View flyer</IonTitle>
-        </IonTitle>
-      </IonToolbar>
-    </IonHeader>
-<IonLoading
-            isOpen={true}
-            message={"Please wait..."}
-            onDidDismiss={() => setShowLoading(false)}
-            />
-        </IonContent>
-  )
-}
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>
+            <IonTitle>Loading</IonTitle>
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      {/* <IonLoading
+        isOpen={true}
+        message={"Please wait..."}
+        onDidDismiss={() => setShowLoading(false)}
+      /> */}
+    </IonContent>
+  );
+};
 
-export default LoadingPage
+export default LoadingPage;
